5683c14a7b34b3c42f5af448ddce84302123349b
Albert Sharapov <elistaphilly@gmail.com>
2019-04-01 18:47:42 -0400

    first commit

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
+let connectionWindow;
 
 app.on('ready', () => {
   // Create main window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow(
+    {
+      webPreferences: {
+        nodeIntegration: true
+      }
+    }
+  );
 
   // Load html into window
   mainWindow.loadURL(url.format({
@@ -17,10 +24,46 @@ app.on('ready', () => {
     slashes: true
   }));
 
+  // Quit app when closed
+  mainWindow.on('closed', () => app.quit());
+
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
   Menu.setApplicationMenu(mainMenu);
+})
+
+// handle create connection window
+
+const handleConnectionWindowOpen = () => {
+  connectionWindow = new BrowserWindow(
+    {
+      width: 300,
+      height: 200,
+      title: 'Establish TCP connection',
+      webPreferences: {
+        nodeIntegration: true
+      }
+    }
+  );
+
+  // Load html into window
+  connectionWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'connectionWindow.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  // Garbage collection handle
+  connectionWindow.on('close', () => {
+    connectionWindow = null;
+  })
+}
+
+// Catch connection:port:ip
+ipcMain.on('connection:port:ip', (e, host) => {
+  console.log(e);
+  console.log(host);
 })
 
 // Create menu template
@@ -30,9 +73,7 @@ const mainMenuTemplate = [
     submenu: [
       {
         label: 'Connect',
-        // click: () => {
-        //   createConnectWindow();
-        // }
+        click: () => handleConnectionWindowOpen()
       },
       {
         label: 'Disconnect'
@@ -45,3 +86,27 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+// If MAC, add empty object to menu
+if(process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools
+if(process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push(
+    {
+      label: 'Developer Tools',
+      submenu: [
+        {
+          label: 'Toggle DevTools',
+          accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+          click: (item, focusedWindow) => focusedWindow.toggleDevTools()
+        },
+        {
+          role: 'reload'
+        }
+      ]
+    }
+  );
+}
